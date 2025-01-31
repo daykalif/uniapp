ae0bf7fdca28eb8f06523435a2b525251ecb3799
daykalif <1038200595@qq.com>
2025-01-31 22:43:34 +0800

    feat: uniapp如何使用vue3编写页面

---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,47 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const common_assets = require("../../common/assets.js");
+const _sfc_main = {
+  __name: "index",
+  setup(__props) {
+    const title = common_vendor.ref("Hello");
+    const list = common_vendor.reactive([
+      {
+        name: "apple",
+        num: 1
+      },
+      {
+        name: "banana",
+        num: 2
+      }
+    ]);
+    common_vendor.onLoad(() => {
+      common_vendor.index.__f__("log", "at pages/index/index.vue:41", "onLoad生命周期");
+    });
+    const handleClick = () => {
+      list.forEach((item) => {
+        item.num++;
+      });
+    };
+    const totalNum = common_vendor.computed(() => {
+      return list.reduce((total, cur) => total + cur.num, 0);
+    });
+    return (_ctx, _cache) => {
+      return {
+        a: common_assets._imports_0,
+        b: common_vendor.t(title.value),
+        c: common_vendor.f(list, (item, k0, i0) => {
+          return {
+            a: common_vendor.t(item.name),
+            b: common_vendor.t(item.num),
+            c: item.name
+          };
+        }),
+        d: common_vendor.o(handleClick),
+        e: common_vendor.t(totalNum.value)
+      };
+    };
+  }
+};
+wx.createPage(_sfc_main);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/index/index.js.map
